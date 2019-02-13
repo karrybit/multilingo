ff9702e46af4aa9c629b83c05bf6ffb747b1559f
Takumi Karibe <takumi.k.5610@gmail.com>
2019-02-14 00:53:24 +0900

    post dummy code without input

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	values := url.Values{}
+	values.Add("language", "swift")
+	values.Add("api_key", "guest")
+	values.Add("source_code", "print(%22hello%22)")
+
+	resp, err := http.PostForm("http://api.paiza.io:80/runners/create", values)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		fmt.Println(string(b))
+	}
+}
