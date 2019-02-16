eced6201cdf9ba556b09ca41ee5e3e223a31b25c
Takumi Karibe <takumi.k.5610@gmail.com>
2019-02-17 02:35:06 +0900

    fix logger

--- Log/Log.go
+++ Log/Log.go
@@ -12,7 +12,7 @@ type Logger interface {
 func PrintFields(target Logger) {
 	v := reflect.Indirect(reflect.ValueOf(target))
 	t := v.Type()
-	fmt.Printf("%v {\n", t)
+	fmt.Printf("📦 %v {\n", t)
 	for i := 0; i < t.NumField(); i++ {
 		fmt.Printf("\t%-15s%v\n", t.Field(i).Name, v.Field(i))
 	}
