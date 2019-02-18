d325b28429441103a1fa715da7f251bf8917b2bd
Takumi Karibe <takumi.k.5610@gmail.com>
2019-02-19 02:41:32 +0900

    test parse program

--- parseRawText/Parse_test.go
+++ parseRawText/Parse_test.go
@@ -2,7 +2,8 @@ package parserawtext
 
 import "testing"
 
-func TestNormalLanguage(t *testing.T) {
+func TestParseLanguageNormal(t *testing.T) {
+	// map[string]string{argument: expected}
 	langMap := map[string]string{"UG6LTEJBV": "swift"}
 	for k, v := range langMap {
 		lang, err := lookUp(k)
@@ -11,6 +12,17 @@ func TestNormalLanguage(t *testing.T) {
 		}
 		if lang != v {
 			t.Errorf("lookUp not %s, got= %s", v, lang)
+		}
+	}
+}
+
+func TestParseProgramNormal(t *testing.T) {
+	// map[string]string{argument: expected}
+	programMap := map[string]string{"```print(\"hello world\")```": "print(\"hello world\")"}
+	for k, v := range programMap {
+		program := parseProgram(k)
+		if program != v {
+			t.Errorf("parseProgram not %s, got= %s", v, program)
 		}
 	}
 }
