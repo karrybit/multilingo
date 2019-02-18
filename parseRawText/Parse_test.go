2d59f28fd5360d599f9b8e8ead0c5a5daaa8c01e
Takumi Karibe <takumi.k.5610@gmail.com>
2019-02-19 02:54:40 +0900

    test Parse

--- parseRawText/Parse_test.go
+++ parseRawText/Parse_test.go
@@ -27,3 +27,27 @@ func TestParseProgramNormal(t *testing.T) {
 		}
 	}
 }
+
+func TestParseNormal(t *testing.T) {
+	// map[string]string{argument: expected}
+	textMap := map[string]Pair{"<@UG6LTEJBV>\n```print(114514)```\n": Pair{"swift", "print(114514)"}}
+	for k, v := range textMap {
+		lang, program, err := Parse(k)
+		if err != err {
+			t.Error(err)
+		}
+
+		if lang != v.lang {
+			t.Errorf("Parse language %s, got= %s", v.lang, lang)
+		}
+
+		if program != v.program {
+			t.Errorf("Parse program %s, got= %s", v.program, program)
+		}
+	}
+}
+
+type Pair struct {
+	lang    string
+	program string
+}
